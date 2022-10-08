044628c93e6fbdb11a108d2e87f02399a3bdfbb8
Rishabh Nahar <rishabhnahar1202@gmail.com>
2022-10-08 09:19:23 +0530

    commit

--- extension/funcs.js
+++ extension/funcs.js
@@ -5,18 +5,20 @@ function getCurrentTabUrl () {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
         if(url.includes('https')){
-            alert("site is safe")
-            
+            // alert("site is safe")
         }
         else{
             alert("site is not safe")
             url.href = "www.google.com"
         }
+        window.location.replace(window.location.href);
         var req = new XMLHttpRequest();
             req.open('GET', url, false);
             req.send(null);
             var headers = req.getAllResponseHeaders().toLowerCase();
-            alert(headers);
-    });    
+            // alert(headers);
+});    
   }
 
+chrome.extension.onRequest.addListener(function(request, sender) {
+});
